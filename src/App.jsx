1d8e7eb458eafac73b757269ecd2983d927a85bf
Andrei Harenkov <pevec1@yandex.ru>
2024-07-03 14:26:09 +0300

    update 03-07-2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './index.css'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Menu() {
   return (
     <nav className="menu">
-      <a className="menu__item" href="/">
+      <Link className="menu__item" to={"/"}>
         Главная
-      </a>
-      <a className="menu__item" href="/drift">
+      </Link>
+      <Link className="menu__item" to={"/drift"}>
         Дрифт-такси
-      </a>
-      <a className="menu__item" href="/timeattack">
+      </Link>
+      <Link className="menu__item" to={"/timeattack"}>
         Time Attack
-      </a>
-      <a className="menu__item" href="/forza">
+      </Link>
+      <Link className="menu__item" to={"/forza"}>
         Forza Karting
-      </a>
+      </Link>
     </nav>
   );
 }
@@ -26,9 +26,9 @@ function Menu() {
 function HomePage() {
   return (
     <article className="article">
-      <h1 className="article__title">Гоночное такси</h1>
+      <h1 className="article__title">Гоночн​ое такси</h1>
       <p className="article__paragraph">
-        Гоночное такси – отличная возможность насладиться скоростью и мастерством гонщика,
+        Гоночн​ое такси – отличная возможность насладиться скоростью и мастерством гонщика,
         сидя на месте штурмана, и стать свидетелем настоящего мастерства профессиональных
         инструкторов Сочи Автодрома, в полной мере ощутив крутые виражи на самой современной
         гоночной трассе России.
@@ -42,7 +42,7 @@ function DriftPage() {
     <article className="article">
       <h1 className="article__title">Дрифт-такси</h1>
       <p className="article__paragraph">
-        Только на Сочи Автодроме вас ждет уникальная возможность
+        Только ​на Сочи Автодроме вас ждет уникальная возможность
         промчаться по трассе Формулы 1 на максимально возможной
         скорости в управляемом заносе на легендарной «королеве дрифта» Nissan Silvia!
       </p>
@@ -76,7 +76,7 @@ function ForzaPage() {
 function TimeAttackPage() {
   return (
     <article className="article">
-      <h1 className="article__title">Гонка Time Attack</h1>
+      <h1 className="article__title">Гонка ​​​​​​Time Attack</h1>
       <p className="article__paragraph">
         Гонка Time Attack создана для тех, кто любит ставить рекорды –
    открытое соревнование на гражданских автомобилях не предполагает
@@ -99,15 +99,15 @@ export default function App() {
       <div>
         <Menu />
         <div className="page">
-          <Switch>
-            <Route path="/" element={<HomePage />} />
+          <Routes>
+            <Route path="/" exact element={<HomePage />} />
             <Route path="/drift" element={<DriftPage />} />
             <Route
               path="/timeattack"
               element={<TimeAttackPage />}
             />
             <Route path="/forza" element={<ForzaPage />} />
-          </Switch>
+          </Routes>
         </div>
       </div>
     </Router>
